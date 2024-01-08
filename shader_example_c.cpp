02f6e959474bb080269d6ba3adaca693cdee6d21
sourland <jem2010@windowslive.com>
2024-01-08 22:23:44 +0200

    add a shader class

--- shader_example_c.cpp
+++ shader_example_c.cpp
@@ -10,25 +10,6 @@ void processInput(GLFWwindow *window);
 
 const GLint WIDTH = 800, HEIGHT = 600;
 
-
-const char *vertexShaderSource ="#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "layout (location = 1) in vec3 aColor;\n"
-    "out vec3 ourColor;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos, 1.0);\n"
-    "   ourColor = aColor;\n"
-    "}\0";
-
-const char *fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "in vec3 ourColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(ourColor, 1.0f);\n"
-    "}\n\0";
-
 int main() {
 
     //Init glfw
@@ -54,7 +35,7 @@ int main() {
         return -1;
     } 
 
-    Shader ourShader("shaders/vertex.vs", "shaders/shader.fs");
+    Shader ourShader("shaders/vertex.vs", "shaders/fragment.fs");
 
     /* Code Begin */
     float vertices[] = {
@@ -77,7 +58,7 @@ int main() {
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    
+    float offset = 0.5f;
     
     /* Code End */
      
@@ -89,6 +70,7 @@ int main() {
 
         /* Code Begin */
         ourShader.use();
+        ourShader.setFloat("offset", offset);
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         /* Code End */
